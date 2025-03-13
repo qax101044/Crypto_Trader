c7688537f2db6b86f2f78d9f9ea74227ad7319e7
KimHongHee <qaz101044@naver.com>
2025-03-13 21:27:20 +0900

    chore: 프로젝트 초기 설정 및 Git 연결

---
+++ src/main/java/com/cryptosimulator/cryptosimulator/CryptoSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.cryptosimulator.cryptosimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptosimulator/cryptosimulator/CryptoSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptosimulator.cryptosimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
